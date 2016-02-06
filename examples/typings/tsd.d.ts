dad15675423ce832768042b7d1841564167c003a
Hiroyuki Wada <wadahiro@gmail.com>
2016-02-07 00:07:53 +0900

    moving to typings from tsd

--- examples/typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="react/react-dom.d.ts" />
-/// <reference path="react/react.d.ts" />
-/// <reference path="react-bootstrap/react-bootstrap.d.ts" />
-/// <reference path="node/node.d.ts" />
